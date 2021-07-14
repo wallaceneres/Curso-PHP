a46630ddb96760f8fd00045802a0366110c52bf4
wallaceneres <wallace.souzza@live.com>
2021-07-14 16:12:32 -0300

    metodos de lista

--- DAO/index.php
+++ DAO/index.php
@@ -2,16 +2,37 @@
 
 require_once ("config.php");
 
-//$sql = new Sql();
+/*retorna a lista de usuarios
+$sql = new Sql();
 
-//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+echo json_encode($usuarios);
+*/
 
-//echo json_encode($usuarios);
-
+/*retorna um usuario especifico enviado atraves do metodo loadById
 $teste = new Usuario();
 
 $teste->loadByID(3);
 
-echo $teste;
+echo $teste;*/
+
+//tambem retorna uma lista de usuarios
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+//carrega uma lista de usuarios buscando pelo login
+
+/*$busca = Usuario::search("se");
+
+echo json_encode($busca);*/
+
+//carrega os dados do usuario usando login e senha
+
+$entrar = new Usuario();
+
+$entrar->login("user", "123456");
+
+echo $entrar;
 
 ?>
